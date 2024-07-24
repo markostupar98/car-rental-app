81377c7d8f9e08247eb0365217808106bfe24679
markostupar98 <markostuparets@gmail.com>
2024-07-24 18:02:03 +0200

    adding middleware auth auth config and next auth

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("Is logged in", isLoggedIn);
+  // req.auth
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
