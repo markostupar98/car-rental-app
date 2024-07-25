df128323b0af76c97818e91ea013b4a19bfb43e1
markostupar98 <markostuparets@gmail.com>
2024-07-25 19:40:10 +0200

    adding role based authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,42 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./lib/db";
+import { getUserById } from "./services/user";
 import authConfig from "./auth.config";
+
+declare module "@auth/core" {
+  interface session {
+    user: { role } & DefaultSession["user"];
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      try {
+        const existingUser = await getUserById(token.sub);
+
+        if (!existingUser) return token;
+
+        token.role = existingUser.role;
+      } catch (error) {
+        console.error("Error in JWT callback:", error);
+      }
+
+      return token;
+    },
+  },
 });
---
+++ src/services/user.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/db";
+
+export async function getUserById(userId: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw new Error("Could not fetch user");
+  }
+}
